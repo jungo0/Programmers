ca3267a0d4b8b41f6fd683ce15535dfe6a82bb41
jungo <mkk90645@gmail.com>
2023-03-15 22:06:00 +0900

    Programmers

--- Lv_0/test.js
+++ Lv_0/test.js
@@ -1,11 +1,26 @@
-function getLengthOfLongestElement(arr) {
-    return arr.reduce(function(num1, num2){
-      if (num1.length >= num2.length) {
-        return num1.length;
-      } else {
-        return num2.length;
-    }},0);
-  }
-
-  let output = getLengthOfLongestElement(['one', 'two', 'three']);
-  console.log(output); // --> 5
+const cartoons = [
+    {
+      id: 1,
+      bookType: 'cartoon',
+      title: '식객',
+      subtitle: '어머니의 쌀',
+      createdAt: '2003-09-09',
+      genre: '요리',
+      artist: '허영만',
+      averageScore: 9.66,
+    },
+    {
+      id: 2,
+      // .. 이하 생략
+    },
+    // ... 이하 생략
+  ]; 
+  
+  // 단행본 한 권의 출판 연도가 2003인지 확인하는 함수
+  const isCreatedAt2003 = function (cartoon) {
+    const fullYear = new Date(cartoon.createdAt).getFullYear()
+    return fullYear === 2003;
+  }; 
+  
+  // 출판 연도가 2003년인 책의 모음
+  const filteredCartoons = cartoons.filter(isCreatedAt2003); 
