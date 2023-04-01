15ef5de0919eeb6dd906559d9011104efa65a106
jungo <mkk90645@gmail.com>
2023-04-01 18:03:35 +0900

    치킨쿠폰

---
+++ Lv_0/chickent_coupon.js
@@ -0,0 +1,13 @@
+function solution(chicken) {
+    // 쿠폰은 치킨 수 만큼
+    let coupon = chicken
+    // 서비스 받은 치킨 수
+    let service = 0
+    // 쿠폰이 10개 이상이라면
+    while(coupon >= 10) {
+        service+=coupon/10 << 0
+        // 남은 쿠폰 = coupon%10, 서비스로 주문한 치킨의 쿠폰 수 = coupon/10
+        coupon = coupon%10 + coupon/10 << 0
+    }
+    return service
+}
